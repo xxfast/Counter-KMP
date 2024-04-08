3a177148fb01ffafc2c15298d66d36e69c483787
xxfast <isurukusumal36@gmail.com>
2024-04-08 21:51:15 +1000

    Remove boilerplate

--- counter/src/androidMain/kotlin/io/github/xxfast/counter/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package io.github.xxfast.counter
-
-class AndroidPlatform : Platform {
-  override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- counter/src/commonMain/kotlin/io/github/xxfast/counter/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package io.github.xxfast.counter
-
-class Greeting {
-  private val platform: Platform = getPlatform()
-
-  fun greet(): String {
-    return "Hello, ${platform.name}!"
-  }
-}
