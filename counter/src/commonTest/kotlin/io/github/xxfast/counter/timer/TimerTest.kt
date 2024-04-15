dcd45faec9626a939e5eea06cfbe0556137dd8a9
xxfast <isurukusumal36@gmail.com>
2024-04-16 00:18:13 +1000

    Add timer tests

--- counter/src/commonTest/kotlin/io/github/xxfast/counter/timer/TimerTest.kt
+++ counter/src/commonTest/kotlin/io/github/xxfast/counter/timer/TimerTest.kt
@@ -4,19 +4,37 @@ import app.cash.molecule.RecompositionMode
 import app.cash.molecule.moleculeFlow
 import app.cash.turbine.test
 import io.github.xxfast.counter.screens.timer.TimerDomain
+import io.github.xxfast.counter.screens.timer.TimerState
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class TimerTest {
   @Test
-  fun `Test timer stops at zero`() = runTest {
-    moleculeFlow(RecompositionMode.Immediate) { TimerDomain(60) }
+  fun `Test minute rollover`() = runTest {
+    moleculeFlow(RecompositionMode.Immediate) { TimerDomain() }
       .test {
-        for(expect in 60 downTo 0){
-          val actual: Int = awaitItem()
-          assertEquals(expect, actual)
-        }
+        assertEquals(
+          expected = TimerState(1, 0),
+          actual = awaitItem()
+        )
+
+        assertEquals(
+          expected = TimerState(0, 59),
+          actual = awaitItem()
+        )
+      }
+  }
+
+  @Test
+  fun `Test timer stops at zero`() = runTest {
+    moleculeFlow(RecompositionMode.Immediate) { TimerDomain() }
+      .test {
+        repeat(60) { this.awaitItem() }
+        assertEquals(
+          expected = TimerState(0, 0),
+          actual = awaitItem()
+        )
       }
   }
 }
